b214b8cb444f6b5cf0eb87bb0c692605142b3319
Adriana <dricabd@gmail.com>
2020-07-16 11:26:44 -0300

    Subindo exercicios salvos. Final

---
+++ javaScript.js
@@ -0,0 +1,43 @@
+//6.1 As variáveis podem mudar
+Ex: var a = 'adriana' (a variável a recebeu o nome adriana)
+// Se eu nao quiser mais esse nome, posso trocar.
+var a = 10
+// Já a constante, não poderá ser modificada
+cont b = 20 
+//Sempre será 20. Não pode ser modificada
+
+//6.2 Os comandos de decisoes servem para tomar diferentes caminhos, dependendo da condição.
+
+var numero = ('Digite um numero');
+if (numero >10){
+    alert('Você digitou numero maior que 10');
+}else{
+    alert('Você digitou numero menor que 10')
+}
+//Ao digitar um numero, se for maior que dez, dará a mensagem de numero maior. E se for menor, dará a outra mensagem.
+
+//6.3 O loop é como fazer a mesma coisa várias vezes. São laços de repetição
+//Exemplo: tenho uma lista e quero saber o tamamho dela. Vou dar um comando for, com incremento até cheagar ao final da lista pra ele me listar os nome
+
+//6.4 - Posso criar funções dentro do programa. Exemplo. posso criar a funçao teste e testar algo no javaScript ou função mostrar alerta.
+//6.5 Os escopos são a maneira que tenho de acessar alguma variável ou função que eu criei. Quando crio dentro de uma função, se torna local e quando crio fora, se torna global
+
+//6.6 Inputs são as entradas - prompt, por exemplo e Outputs são saídas como o alert, console.log e document.write
+
+//6.7 Array é quando crio uma lista de algo, por exemplo, listar nomes dos alunos, frutas que tenho em casa
+
+//6.8 Objetos: já os objetos são criados quando tenho vários especificações  dentro. Criado por chaves{}, como quero variavel celular. Dentro de celular tenho marca, modelo, ano fabricação.
+
+// 6.9 padrões de escrita camelCase - tudo minuscula com a segunda palavra maiuscula (camelCase) Usada em javaScript; PascalCase - primeira e segunda letra maiusculas (PascalCase);Snake_case - primeira letra maiuscula e separadas por underline (Snake_case) e a kibab-case - tudo minusculo e separada por traço (kibab-case)
+
+//6.10 Caminho absuluto é o caminho fixo (C:\) e caminho relativo é o caminho com base no que estou executando.
+
+//6.11 Operadores &&: ambas devem ser verdade para ser true; || somente uma deve ser verdade para ser true, == tem que ser igual e != deve ser diferente para ser true
+
+//6.12 EPER: Entender do que se trata o código, Planejar como será feito o código, Executar é colocar realmente códigos para rodas e Revisar é saber se está tudo em perfeitas condições, se está correto.
+
+//6.13 Fila é quando tenho uma situação em que o primeiro que entra é o primeiro que sai. E pilha é o primeiro que entra e o último a sair
+
+//6.14 O html é o que sustenta, é o esqueleto. O css é o que dá a beleza. São os ajustes, toques, detalhes finos e o javascript é o vai dar a vida. É o coração.
+
+//
